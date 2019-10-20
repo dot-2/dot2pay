73cb30a2a055b5b2152fdc79cf718a1dbab5ffc1
noxue <173126019@qq.com>
2019-10-20 22:29:04 +0800

    first commit

---
+++ src/main/java/com/dot2/Dot2Application.java
@@ -0,0 +1,13 @@
+package com.dot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dot2/Dot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
